a18bc7aaf6f6c9a97861ac687b8abfb8a3907995
Dobriiii <dilev8g@gmail.com>
2023-01-21 14:56:58 +0200

    Finishing circle button!

--- Project-Programming/MainForm.Designer.cs
+++ Project-Programming/MainForm.Designer.cs
@@ -108,6 +108,7 @@
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnTriangle);
             this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
--- Project-Programming/MainForm.cs
+++ Project-Programming/MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,7 +17,22 @@ namespace Project_Programming
         {
             InitializeComponent();
         }
-
+        Thread th;
+        Random rdm;
+        public void threadCircle()
+        {
+            while (true)
+            {
+                int width = rdm.Next(0, this.Width);
+                int height = rdm.Next(0, this.Height);
+                if (width < 700 && height < 400)
+                {
+                    int random = rdm.Next(0, 50);
+                    this.CreateGraphics().DrawEllipse(new Pen(Brushes.Black, 1), new Rectangle(width, height, random, random));
+                    Thread.Sleep(400);
+                }
+            }
+        }
         private void btnTriangle_Click(object sender, EventArgs e)
         {
 
@@ -29,7 +45,8 @@ namespace Project_Programming
 
         private void btnCircle_Click(object sender, EventArgs e)
         {
-
+            th = new Thread(threadCircle);
+            th.Start();
         }
 
         private void btnLines_Click(object sender, EventArgs e)
@@ -45,6 +62,11 @@ namespace Project_Programming
         private void btnStop_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            rdm = new Random(); 
         }
     }
 }
